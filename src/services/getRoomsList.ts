aa5b7258cb0f82345ff41a4d4d9503bad3afdb0a
Wenderson Pires <wendersonpdas@gmail.com>
2023-07-24 17:32:21 -0300

    log removed

--- src/services/getRoomsList.ts
+++ src/services/getRoomsList.ts
@@ -2,7 +2,6 @@ import { Room } from "@app/database/history-tables/Rooms";
 import { api } from "./api";
 
 const getRoomsList = async () => {
-  console.log(`${window.location.origin}/api/room/list`);
   const rooms = await api().get<Room[]>(
     `${window.location.origin}/api/room/list`
   );

fef9eefdfcd3fdf8f99c0edffd1e19cfd0d9e8d9
Wenderson Pires <wendersonpdas@gmail.com>
2023-07-27 19:34:03 -0300

    get history depth changed

--- src/pages/api/message/[room_id].ts
+++ src/pages/api/message/[room_id].ts
@@ -9,7 +9,7 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   }
 
   const messages = await messagesHistoryTable(roomId);
-  const historyMsg = await messages.getHistory(100);
+  const historyMsg = await messages.getHistory(1000);
 
   // Newest messages first
   const sortedMessages = historyMsg.sort(
